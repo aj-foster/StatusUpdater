d050b99ca2c7fe30ccf69e8a5778e8fb7e5f9ae3
AJ Foster <web@aj-foster.com>
2017-10-31 14:51:41 -0400

    Initial commit with escript enabled and poison installed

---
+++ lib/status_updater.ex
@@ -0,0 +1,23 @@
+defmodule StatusUpdater do
+  @moduledoc """
+  Documentation for StatusUpdater.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StatusUpdater.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+  
+  
+  def main (args) do
+    IO.puts("Hello, cruel world")
+  end
+end
---
+++ test/status_updater_test.exs
@@ -0,0 +1,8 @@
+defmodule StatusUpdaterTest do
+  use ExUnit.Case
+  doctest StatusUpdater
+
+  test "greets the world" do
+    assert StatusUpdater.hello() == :world
+  end
+end
