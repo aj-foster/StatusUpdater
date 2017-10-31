07d42ac16a350759294b963418d263ea6e787b79
AJ Foster <web@aj-foster.com>
2017-10-31 15:40:29 -0400

    Add docs and a "clear status" preset

--- lib/status_updater.ex
+++ lib/status_updater.ex
@@ -1,13 +1,25 @@
 defmodule StatusUpdater do
   @moduledoc """
-  Documentation for StatusUpdater.
+  This module is the entrypoint for an escript binary. It calls out to the
+  Slack API using a token defined in `config.exs` in order to update a user's
+  status message and emoji.
   """
 
 
+  @doc """
+  Entrypoint for the escript binary.
+
+  When the generated binary is run, this function will be called with an array
+  containing strings representing the arguments passed to the binary. Using
+  these arguments, it will delegate to update/2 to call the Slack API.
+  """
   def main (args) do
     args
     |> Enum.at(0)
     |> case do
+      "clear" ->
+        update("", "")
+
       "lunch" ->
         update("Lunch", ":fork_and_knife:")
 
@@ -16,7 +28,13 @@ defmodule StatusUpdater do
     end
   end
 
-  defp update(text, emoji \\ "") do
+
+  @doc """
+  Calls the Slack API to update the configured user's status.
+
+  Which user is updated depends on the token placed in `config.exs`.
+  """
+  def update(text, emoji \\ "") do
     url = "https://slack.com/api/users.profile.set"
     token = Application.get_env(:status_updater, :token)
     body = {:form, [{"profile", "{\"status_text\":\"#{text}\",\"status_emoji\":\"#{emoji}\"}"}, {"token", token}]}
