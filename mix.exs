9a0f32be0ad1953853a9c1a2855d5bce94a19e34
AJ Foster <web@aj-foster.com>
2017-10-31 15:14:40 -0400

    Add HTTPoison for making external requests

--- mix.exs
+++ mix.exs
@@ -15,13 +15,14 @@ defmodule StatusUpdater.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:httpoison, :logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 0.13"},
       {:poison, "~> 3.1"}
     ]
   end
