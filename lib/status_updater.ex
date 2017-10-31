0070a5f3053ee48bd6d1741c8638b9676ff4d7fd
AJ Foster <web@aj-foster.com>
2017-10-31 15:33:33 -0400

    Change test function to update(), experiment with presets

--- lib/status_updater.ex
+++ lib/status_updater.ex
@@ -3,17 +3,25 @@ defmodule StatusUpdater do
   Documentation for StatusUpdater.
   """
 
-  def test do
+
+  def main (args) do
+    args
+    |> Enum.at(0)
+    |> case do
+      "lunch" ->
+        update("Lunch", ":fork_and_knife:")
+
+      "meeting" ->
+        update("In a Meeting", ":calendar:")
+    end
+  end
+
+  defp update(text, emoji \\ "") do
     url = "https://slack.com/api/users.profile.set"
     token = Application.get_env(:status_updater, :token)
-    body = {:form, [{"profile", "{\"status_text\":\"Testing\"}"}, {"token", token}]}
+    body = {:form, [{"profile", "{\"status_text\":\"#{text}\",\"status_emoji\":\"#{emoji}\"}"}, {"token", token}]}
     headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
-    
+
     HTTPoison.post(url, body, headers)
-  end
-  
-  
-  def main (args) do
-    test()
   end
 end
